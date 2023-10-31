1212714368eecde239166e1a0b0baef35dc88917
airineidaniel23 <airineidaniel23@gmail.com>
2023-10-31 23:36:35 +0200

    Center team

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -4,7 +4,7 @@ import PersonCard from './PersonCard';
 const Team = ({ peopleData }) => {
   
   return (
-    <div style={{ display: 'flex' }}>
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
       {peopleData.map((person, index) => (
         <div key={index} style={{ margin: '10px' }}>
           <PersonCard person={person} />
